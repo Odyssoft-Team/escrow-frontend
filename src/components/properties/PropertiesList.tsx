6b880f4054468a1262ee5b287d1051922b1c4b43
Aldo <aldo.parra.dev@gmail.com>
2025-08-30 20:15:22 -0500

    update oficce

--- src/components/properties/PropertiesList.tsx
+++ src/components/properties/PropertiesList.tsx
@@ -5,21 +5,45 @@ import { Property } from "@/types/property";
 import PropertyCard from "@/components/properties/PropertyCard";
 import { ChevronLeft, Search, X } from "lucide-react";
 import { Input } from "../ui/input";
+import { MdPinDrop, MdTag } from "react-icons/md";
 
 import {
   Drawer,
   DrawerClose,
   DrawerContent,
+  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
 } from "@/components/ui/drawer";
 import { Button } from "../ui/button";
-import { FaBuilding, FaFlag, FaRegBuilding } from "react-icons/fa";
-import { FaCircleCheck } from "react-icons/fa6";
-import { cn, formatToShortDate } from "@/lib/utils";
+import {
+  FaBuilding,
+  FaFlag,
+  FaLocationArrow,
+  FaRegBuilding,
+  FaRegMap,
+  FaSuitcase,
+} from "react-icons/fa";
+import { FaCircleCheck, FaGear } from "react-icons/fa6";
+import {
+  cn,
+  formatCurrency,
+  formatToDateTime,
+  formatToShortDate,
+} from "@/lib/utils";
 import { BsExclamationCircle } from "react-icons/bs";
 import { IoIosCalendar } from "react-icons/io";
-import { LuText } from "react-icons/lu";
+import { LuCalendarCog, LuText } from "react-icons/lu";
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { TbLocation, TbWorldLatitude, TbWorldLongitude } from "react-icons/tb";
+import {
+  PiCityLight,
+  PiClockCounterClockwiseBold,
+  PiUserCircleFill,
+  PiUserCircleGearFill,
+} from "react-icons/pi";
+import { FiFileText, FiHash } from "react-icons/fi";
+import { RiEdit2Fill } from "react-icons/ri";
 
 interface PropertiesListProps {
   properties: Property[];
@@ -124,7 +148,7 @@ export function PropertiesList({ properties }: PropertiesListProps) {
               </DrawerTitle>
             </div>
           </DrawerHeader>
-          <div className="bg-[#F7F8FA] w-full h-full p-4 flex flex-col items-center gap-4">
+          <div className="bg-[#F7F8FA] w-full h-full p-4 pb-[4rem] flex flex-col items-center gap-4 overflow-y-auto">
             <h2 className="font-bold text-primary text-2xl">
               {propertySelected?.property_name}
             </h2>
@@ -133,7 +157,8 @@ export function PropertiesList({ properties }: PropertiesListProps) {
               <h3
                 className={cn(
                   "flex items-center gap-2 text-lg font-medium",
-                  propertySelected?.property_status === "Available"
+                  propertySelected?.property_status.toLowerCase() ===
+                    "available"
                     ? "text-green-500"
                     : "text-red-500"
                 )}
@@ -145,7 +170,8 @@ export function PropertiesList({ properties }: PropertiesListProps) {
               <div
                 className={cn(
                   "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4",
-                  propertySelected?.property_status === "Available"
+                  propertySelected?.property_status.toLowerCase() ===
+                    "available"
                     ? "bg-green-100"
                     : "bg-red-100"
                 )}
@@ -153,7 +179,8 @@ export function PropertiesList({ properties }: PropertiesListProps) {
                 <FaCircleCheck
                   className={cn(
                     "size-5",
-                    propertySelected?.property_status === "Available"
+                    propertySelected?.property_status.toLowerCase() ===
+                      "available"
                       ? "text-green-500"
                       : "text-red-500"
                   )}
@@ -163,7 +190,8 @@ export function PropertiesList({ properties }: PropertiesListProps) {
                   <span
                     className={cn(
                       "text-base",
-                      propertySelected?.property_status === "Available"
+                      propertySelected?.property_status.toLowerCase() ===
+                        "available"
                         ? "text-green-500"
                         : "text-red-500"
                     )}
@@ -243,8 +271,249 @@ export function PropertiesList({ properties }: PropertiesListProps) {
                   </span>
                 </div>
               </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <AiOutlineDollarCircle className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Monthly Rent</span>
+                  <span className={cn("text-base")}>
+                    {formatCurrency(
+                      propertySelected?.property_rental_amount as number
+                    )}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full p-4 flex flex-col items-start gap-4 bg-white border rounded-xl">
+              <h3
+                className={cn(
+                  "flex items-center gap-2 text-lg font-medium text-blue-500"
+                )}
+              >
+                <FaLocationArrow className="size-5" />
+                Address Information
+              </h3>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <TbLocation className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Address Line 1</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_address1}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <PiCityLight className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">City</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_city}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <FaRegMap className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">State</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_state}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <FiHash className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">ZIP Code</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_postal_code}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full p-4 flex flex-col items-start gap-4 bg-white border rounded-xl">
+              <h3
+                className={cn(
+                  "flex items-center gap-2 text-lg font-medium text-orange-400"
+                )}
+              >
+                <MdPinDrop className="size-5" />
+                Location Coordinates
+              </h3>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <TbWorldLatitude className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Latitude</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_xcoord}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <TbWorldLongitude className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Longitude</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_ycoord}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full p-4 flex flex-col items-start gap-4 bg-white border rounded-xl">
+              <h3
+                className={cn(
+                  "flex items-center gap-2 text-lg font-medium text-green-500"
+                )}
+              >
+                <MdPinDrop className="size-5" />
+                Parties Information
+              </h3>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <PiUserCircleGearFill className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Current Tenant</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.tenant_id || "No tenant assigned"}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <PiUserCircleFill className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Landlord</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.landlord_id}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <FaSuitcase className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Broker</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.broker_id}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full p-4 flex flex-col items-start gap-4 bg-white border rounded-xl">
+              <h3
+                className={cn(
+                  "flex items-center gap-2 text-lg font-medium text-purple-500"
+                )}
+              >
+                <FaGear className="size-5" />
+                System Information
+              </h3>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <MdTag className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Property ID</span>
+                  <span className={cn("text-base")}>
+                    {propertySelected?.property_id}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <LuCalendarCog className={cn("size-5 text-primary")} />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Created</span>
+                  <span className={cn("text-base")}>
+                    {formatToDateTime(propertySelected?.created_at || "")}
+                  </span>
+                </div>
+              </div>
+
+              <div
+                className={cn(
+                  "w-full px-4 py-2 border rounded-xl flex items-center justify-start gap-4"
+                )}
+              >
+                <PiClockCounterClockwiseBold
+                  className={cn("size-5 text-primary")}
+                />
+                <div className="flex flex-col gap-0 leading-[1]">
+                  <span className="text-content text-sm">Last Updated</span>
+                  <span className={cn("text-base")}>
+                    {formatToDateTime(propertySelected?.updated_at || "")}
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
+
+          <DrawerFooter className="border-t flex-row justify-center gap-4">
+            <Button className="bg-primary text-white h-12">
+              {" "}
+              <RiEdit2Fill /> Edit Property
+            </Button>
+            <Button className="bg-green-400 text-white h-12">
+              {" "}
+              <FiFileText /> Create Contract
+            </Button>
+          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </div>
